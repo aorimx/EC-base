defea2b788357a934cf325ac39d3a2850a5b610c
inigo98 <navarretigo12@gmail.com>
2018-01-30 12:20:08 -0600

    develop
    contenedores de texto y cita

--- public_html/includes/templates/ecbase.catalog/pages/page1.inc.php
+++ public_html/includes/templates/ecbase.catalog/pages/page1.inc.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-
-
-<body>
   <!--  Header  -->
   <div class="header-container">
 
@@ -18,21 +9,14 @@
       <p>And I'm a Photographer</p>
       <button class="ripple">Hire me</button>
     </div>
-  </div>-->∫
+  </div>-->
   <!--  Main content -->
   <main class="j-workspace ">
     <div class="j-wrap">
     <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/Componente1.inc.php'); ?>
     <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/Componente2.inc.php'); ?>
     <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/Componente3.inc.php'); ?>
+    <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/Componente4.inc.php'); ?>
+    <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/Componente5.inc.php'); ?>
     </div>
   </main>
-  <footer>
-  </footer>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="assets/scripts/ripple.js"></script>
-  <script src="assets/scripts/header.js"></script>
-
-</body>
-</html>
