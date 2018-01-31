fa02aa1f5cd2eb538afbd52177c920eaaabdd7ea
inigo98 <navarretigo12@gmail.com>
2018-01-31 13:47:42 -0600

    WIP-Inigo
    arreglos

---
+++ public_html/includes/boxes/Componente1.inc.php
@@ -0,0 +1,9 @@
+<?php
+    $Componente1 = new view();
+    $Componente1->snippets = array(
+      'titulo' => "Titulo2222",
+      'parrafo' => "parrafito",
+      'boton' => "DFZGGSFGV",
+    );
+    return $Componente1->stitch('views/Componente1');
+ ?>
