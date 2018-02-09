00078edaa46116304cdf00e1e465542d60e5f66c
AndreaPAbundis <lizbethabundispgmail.com>
2018-02-09 16:20:19 -0600

    WIP-Andrea
    Componente 1 en admin, proceso

---
+++ public_html/components/Componente1.inc.php
@@ -0,0 +1,10 @@
+<div id="Componente1" class="container-comp1">
+  <div class="img-container">
+    <img src="https://dummyimage.com/500x300/000/fff"></img>
+  </div>
+  <div class="description">
+    <h1><?php echo $data['title'] ?></h1>
+    <h3><?php echo $data['subtitle'] ?></h3>
+    <a href="<?php echo $data['btn_link'] ?>"><button><?php echo $data['btn_title']; ?></button></a>
+  </div>
+</div>
