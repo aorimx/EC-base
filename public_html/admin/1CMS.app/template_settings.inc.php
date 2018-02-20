471b5520d28436626921b95622430cd6fc3112a5
Inigo98 <navarretigo12@gmail.com>
2018-02-20 16:32:02 -0600

    WIP-INIGO
    correcion de algunos estilos del admin

--- public_html/admin/1CMS.app/template_settings.inc.php
+++ public_html/admin/1CMS.app/template_settings.inc.php
@@ -38,7 +38,7 @@
 
 <?php echo functions::form_draw_form_begin('template_settings_form', 'post', null, false, 'style="max-width: 960px;"'); ?>
 
-  <table class="table table-striped data-table">
+  <table class="table table-striped data-table" style="margin-top: 100px;">
     <thead>
       <tr>
         <th style="width: 250px;"><?php echo language::translate('title_key', 'Key'); ?></th>
