8381a943f52ef0bd3d776fa516c21f071bfbadec
AndreaPAbundis <lizbethabundispgmail.com>
2018-03-21 13:49:25 -0600

    WIP-Andrea
    Guardar Componentes automatizado

--- public_html/admin/1CMS.app/component/componente2.inc.php
+++ public_html/admin/1CMS.app/component/componente2.inc.php
@@ -45,7 +45,9 @@
             'btn_link'=>$_POST['btn_link'],
             'imagen'=> 'hero.png',
           );
-          $compoment_content->save((int)$_GET['section_id']);
+          global $var2;
+          $var2=2;
+          $compoment_content->save((int)$_GET['section_id'], (int)$var2);
           notices::add('success', language::translate('success_changes_saved', 'Changes saved'));
           header('Location: '. document::link('', array('app' => $_GET['app'], 'doc' => 'sectiondetail', 'page_id' => $_GET['page_id'], 'section_id'=>$_GET['section_id'])));
           exit;
--- public_html/includes/controllers/ctrl_pages_content_custom.inc.php
+++ public_html/includes/controllers/ctrl_pages_content_custom.inc.php
@@ -43,12 +43,12 @@
         }
 
 
-        public function save($pages_section_custom_id=null){
+        public function save($pages_section_custom_id=null, $v=null){
             if(empty($this->data['id'])){// Si no hay ID es porque es un registro nuevo
                 database::query(
                     "insert into " . DB_TABLE_PAGES_CONTENT_CUSTOM .
                     " (component_content ,position,pages_section_custom_id,pages_component_custom_id)  values " .
-                    "( '" . json_encode($this->data['component_content']) . "','" . $this->data['position']  . "', '". $pages_section_custom_id . "','3')" //ESTE ID SE DEBE CAMBIAR PARA LOS COMPONENTES
+                    "( '" . json_encode($this->data['component_content']) . "','" . $this->data['position']  . "', '". $pages_section_custom_id . "','" . $v ."')" //ESTE ID SE DEBE CAMBIAR PARA LOS COMPONENTES
                 );
                 $this->data['id'] = database::insert_id();
             }else{ //Hay que actualizar
